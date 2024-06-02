91052ce0bcdc4df61c3785622ffa068ee4b49a80
rithw <rithwikpalivela@gmail.com>
2024-06-02 09:32:59 -0700

    added error handling for non google maps page

--- background.js
+++ background.js
@@ -3,11 +3,15 @@ const urlPrefix = "google.com/maps/dir/";
 chrome.action.onClicked.addListener(extensionClicked);
 
 function extensionClicked(tab) {
-    chrome.tabs.sendMessage(tab.id, {
-        type: "NEW",
-        locations: tab.url.substring(tab.url.indexOf(urlPrefix) + urlPrefix.length, tab.url.indexOf('@') - 1).split('/'),
-        currUrl: tab.url.substring(0, tab.url.indexOf('@') - 1)
-    });
+    if (tab.url.includes(urlPrefix)) {
+        chrome.tabs.sendMessage(tab.id, {
+            type: "NEW",
+            locations: tab.url.substring(tab.url.indexOf(urlPrefix) + urlPrefix.length, tab.url.indexOf('@') - 1).split('/'),
+            currUrl: tab.url.substring(0, tab.url.indexOf('@') - 1)
+        });
+    } else {
+        alert("Please only use this extension for Google Maps routes.");
+    }
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
